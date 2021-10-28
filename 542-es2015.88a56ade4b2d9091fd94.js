ac872fa341fb1b01e2761c9d4e514a7d195cd8f2
sreeparnadas <sreeparnadas0675@gmail.com>
2021-10-28 10:13:34 +0530

    first commit

---
+++ 542-es2015.88a56ade4b2d9091fd94.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkaccounts=self.webpackChunkaccounts||[]).push([[542],{42542:function(n,c,t){t.d(c,{vV:function(){return s}}),t(39490),t(38345);var u=t(37716),e=(t(3679),t(72458));t(19238);let s=(()=>{class n{}return n.\u0275fac=function(c){return new(c||n)},n.\u0275mod=u.oAB({type:n}),n.\u0275inj=u.cJS({imports:[[e.BQ,e.si],e.BQ]}),n})()}}]);
