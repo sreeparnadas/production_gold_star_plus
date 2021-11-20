e752cd537a8c0c69ddc43bbf583b6c7c83345dae
sreeparnadas <sreeparnadas0675@gmail.com>
2021-11-20 10:32:17 +0530

    front game page

---
+++ 542-es2015.4d2199fa4acaac9479d4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkaccounts=self.webpackChunkaccounts||[]).push([[542],{2542:function(n,c,t){t.d(c,{vV:function(){return s}}),t(9490),t(8345);var u=t(7716),e=(t(3679),t(2458));t(9238);let s=(()=>{class n{}return n.\u0275fac=function(c){return new(c||n)},n.\u0275mod=u.oAB({type:n}),n.\u0275inj=u.cJS({imports:[[e.BQ,e.si],e.BQ]}),n})()}}]);
